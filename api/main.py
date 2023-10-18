fd11fabcf5b860503ec0b52413be42944ab2ba59
eduardob999 <eduardob1999@gmail.com>
2023-10-17 21:08:17 -0400

    dev multiple apis relase

--- api/main.py
+++ api/main.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from Test import *
 
 app = Flask(__name__)
 
 @app.route('/api/main')
-def test():
-    return 'test'
+def apiMain():
+    return 'main'
+
+@app.route('/api/test')
+def apiTest():
+    answer = test()
+    return answer
