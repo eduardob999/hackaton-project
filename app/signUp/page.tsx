fe38214dc23f7931f6c2cb58c78cd4b59b08e656
eduardob999 <eduardob1999@gmail.com>
2023-10-20 17:36:21 -0400

    dev check up 2

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -1,13 +1,13 @@
-import { SimpleRegistrationForm } from "@/app/signUp/registrationForm"
-import { Suspense } from "react"
-import TablePlaceholder from "@/components/table-placeholder"
+// import { SimpleRegistrationForm } from "@/app/signUp/registrationForm"
+// import { Suspense } from "react"
+// import TablePlaceholder from "@/components/table-placeholder"
 
-export default function Home() {
-  return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <Suspense fallback={<TablePlaceholder />}>
-        {/*<SimpleRegistrationForm />*/}
-      </Suspense>
-    </main>
-  )
-}
+// export default function Home() {
+//   return (
+//     <main className="relative flex min-h-screen flex-col items-center justify-center">
+//       <Suspense fallback={<TablePlaceholder />}>
+//         <SimpleRegistrationForm />
+//       </Suspense>
+//     </main>
+//   )
+// }
