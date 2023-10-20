736030437048763f38ff3dd1f9e8e5c27b853647
eduardob999 <eduardob1999@gmail.com>
2023-10-20 17:31:35 -0400

    dev debug check up 1

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <Suspense fallback={<TablePlaceholder />}>
-        <SimpleRegistrationForm />
+        {/*<SimpleRegistrationForm />*/}
       </Suspense>
     </main>
   )
