e737c1fa38c3a41f80a5d3725e6442678af87cae
eduardob999 <eduardob1999@gmail.com>
2023-10-20 20:56:43 -0400

    dev api2

---
+++ api/utils/testApi2.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/utils/testApi2', methods=['GET'])
+def testApi2():
+    # Access the query parameters from the GET request
+    param1 = request.args.get('param1')
+    param2 = request.args.get('param2')
+
+    # Print the query parameter
+
+    return (f'param1: {param1}, param2: {param2}')
+
+if __name__ == '__main__':
+    app.run()
